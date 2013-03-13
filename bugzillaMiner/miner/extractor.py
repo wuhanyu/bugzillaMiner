06fa4259fd39732433c2b7e554f455f41d259cb2
wuhanyu <wuhanyu@gmail.com>
2013-03-13 16:44:34 +0800

    2

--- bugzillaMiner/miner/extractor.py
+++ bugzillaMiner/miner/extractor.py
@@ -14,4 +14,23 @@ class SequenceExtractor(object):
         '''
         Constructor
         '''
-        
+
+
+class Sample(object):
+    def __init__(self):
+        pass
+    
+    def processFile(self, dom):
+        pass
+    
+    def processHistoryFile(self, dom):
+        pass
+    
+    def __str__(self):
+        result = ''
+        return result
+    
+    def outputCount(self, filepath):
+        file_object = open(filepath, 'w')    
+        file_object.close()
+        return
--- bugzillaMiner/miner/statistician.py
+++ bugzillaMiner/miner/statistician.py
@@ -109,3 +109,18 @@ class TimeStatistician(object):
 class SequenceStatistician(object):
     def __init__(self):
         pass
+    
+    def processFile(self, dom):
+        pass
+    
+    def processHistoryFile(self, dom):
+        pass
+    
+    def __str__(self):
+        result = ''
+        return result
+    
+    def outputCount(self, filepath):
+        file_object = open(filepath, 'w')    
+        file_object.close()
+        return
