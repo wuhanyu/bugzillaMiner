6682da4c17e3cf1fbc677363eee330bff667d501
SimonWu <wuhanyu@gmail.com>
2013-05-03 15:17:38 +0800

    memfunc complete

--- bugzillaMiner/farmer/fstranslator.py
+++ bugzillaMiner/farmer/fstranslator.py
@@ -3,14 +3,12 @@ Created on 2013-5-2
 
 @author: Simon@itechs
 '''
-
-def getEDaysQ(days):
-    if (days < 2):
-        return 1, 0, 0
-    elif (days < 10):
-        return 
+import memfunc
 
 if __name__ == '__main__':
-    input = open('../result/TransitionExctractor_2013-05-02 1542.txt', 'r')
-    for line in input:
-        print line
+#    input = open('../result/TransitionExctractor_2013-05-02 1542.txt', 'r')
+#    for line in input:
+#        print line
+    print memfunc.getMembershipPair(1, memfunc.edays)
+    print memfunc.getMembershipPair(5, memfunc.edays)
+    print memfunc.getMembershipPair(12, memfunc.edays)
