cfa6e2b05ffb724731faaea73cb2feaad2e302fb
SimonWu <wuhanyu@gmail.com>
2013-05-12 16:27:02 +0800

    update memberfunc

--- bugzillaMiner/farmer/fstranslator.py
+++ bugzillaMiner/farmer/fstranslator.py
@@ -6,7 +6,7 @@ Created on 2013-5-2
 import memfunc
 
 if __name__ == '__main__':
-    finname = '../result/SequenceDataExctractor_2013-03-16 1423.txt'
+    finname = '../result/NewSequenceExctractor_2013-05-12_00.txt'
     fin = open(finname, 'r')
     data = fin.read()
     fin.close()
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         if (lcount % 10000 == 0): print "%d lines processed(%.1f%%, %d)" % (lcount, float(lcount)/ linecount * 100, linecount)
         tmp = line.split("\t")
         outputline = ''
-        tmp = tmp[1:6]
+        tmp = tmp[1:]
         for i, item in enumerate(tmp):
             outputline += memfunc.getMembershipPair(memfunc.labels[i][0], item, memfunc.labels[i][1])
         fout.write(outputline[:-1] + "\n")
--- bugzillaMiner/farmer/memfunc.py
+++ bugzillaMiner/farmer/memfunc.py
@@ -8,38 +8,81 @@ mf_edays = [["Short", 0, 1],
          ["Long", 0, 0, 50, 58]
          ]
 
-mf_comments = [["Few", 1, 2],
+mf_comments = [["Small", 1, 2],
          ["Middle", 0, 0, 1, 2, 4, 6],
-         ["Many", 0, 0, 4, 6]
+         ["Large", 0, 0, 4, 6]
          ]
 
-mf_modis = [["Few", 1, 2],
+mf_modis = [["Small", 1, 2],
          ["Middle", 0, 0, 1, 2, 3, 5],
-         ["Many", 0, 0, 3, 5]
+         ["Large", 0, 0, 3, 5]
          ]
 
-mf_umodis = [["Few", 0, 1],
+mf_umodis = [["Small", 0, 1],
          ["Middle", 0, 0, 0, 1, 1, 3],
-         ["Many", 0, 0, 1, 3]
+         ["Large", 0, 0, 1, 3]
          ]
 
-labels = [["Transition", None],
-          ["CommentNum", mf_comments],
-          ["ElapseDays", mf_edays],          
+mf_atn = [["DontHave", 0, 1],
+         ["Have", 0, 0, 1]
+         ]
+
+mf_dev = [["Small", 0, 1],
+         ["Middle", 0, 0, 0, 1, 1, 3],
+         ["Large", 0, 0, 1, 3]
+         ]
+
+mf_severity = [["Low", 0, 1],
+         ["Normal", 0, 0, 3, 4, 4, 5],
+         ["High", 0, 0, 4, 5]
+         ]
+
+mf_depend = [["DontHave", 0, 1],
+         ["Have", 0, 0, 1]
+         ]
+
+mf_block = [["DontHave", 0, 1],
+         ["Have", 0, 0, 1]
+         ]
+
+mf_reputation = [["Low", 0, 25],
+         ["Middle", 0, 0, 25, 41, 41, 52],
+         ["High", 0, 0, 41, 52]
+         ]
+
+mf_fsdays = [["BeforeOpen", 0, 1600],
+         ["DuringOpen", 0, 0, 1600, 1825, 1825, 2190],
+         ["AfterOpen", 0, 0, 1825, 2190]
+         ]
+
+labels = [["LS", None],
+          ["Resolution", None],
+          ["ElapseDays", mf_edays], 
+          ["CommentNum", mf_comments],         
           ["ModiNum", mf_modis],
-          ["UniModiNum", mf_umodis]
+          ["UniModiNum", mf_umodis],
+          ["Attachment", mf_atn],
+          ["DevNum", mf_dev],
+          ["Severity", mf_severity],
+          ["Depend", mf_depend],
+          ["Block", mf_block],
+          ["Reputation", mf_reputation],
+          ["Stage", mf_fsdays],
           ]
 
 def getMembershipValue(value, memfunc):
     count = 1
     isFlat = True
     begin = 1
+#    print value, memfunc
     while (count < len(memfunc) and value >= memfunc[count]):
         count += 1
         isFlat = not isFlat
         if (count % 2 != 0): begin = (begin + 1) % 2
     if (isFlat):
         return begin
+    elif (count == len(memfunc)):
+        return memfunc[-1]
     else:
         end = (begin + 1) % 2
         return float(begin) + float((end - begin) * (value - memfunc[count - 1])) / (memfunc[count] - memfunc[count - 1])
